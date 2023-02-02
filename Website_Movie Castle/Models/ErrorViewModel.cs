baa9272894c12d91f676f033a49567a399d9b4a3
110963 <110963@SMC-LL-126-10>
2023-02-02 15:43:58 +0100

    Add project files.

---
+++ Website_Movie Castle/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Website_Movie_Castle.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
